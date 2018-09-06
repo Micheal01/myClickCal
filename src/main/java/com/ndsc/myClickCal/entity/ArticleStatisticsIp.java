3a11dfdcfd8c37565a8845021db108afdaf86bd4
Micheal01 <33275708+Micheal01@users.noreply.github.com>
2018-09-06 11:56:18 +0800

    developing

--- src/main/java/com/ndsc/myClickCal/entity/ArticleStatisticsIp.java
+++ src/main/java/com/ndsc/myClickCal/entity/ArticleStatisticsIp.java
@@ -23,6 +23,8 @@ public class ArticleStatisticsIp {
     @Column(name = "ID",nullable = false,length=50)
     private String id; //主键
 
+    //以前是用外网ip来限制刷访问量现在改成用session  id来算。这样就不会出现同一个网络下ip一致的问题。
+    //不过这样会出现换浏览器或重开浏览器刷页面也会加访问量的问题。这个不用管
     @Column(name = "IP")
     String ip;//ip 地址
 
--- src/main/java/com/ndsc/myClickCal/util/HttpUtil.java
+++ src/main/java/com/ndsc/myClickCal/util/HttpUtil.java
@@ -12,26 +12,28 @@ public class HttpUtil {
     //获取用户真实IP
     public static String getIp(HttpServletRequest request)
     {
-        String ip = request.getHeader("x-forwarded-for");
-        if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
-            ip = request.getHeader("http_client_ip");
-        }
-        if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
-            ip = request.getRemoteAddr();
-        }
-        if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
-            ip = request.getHeader("Proxy-Client-IP");
-        }
-        if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
-            ip = request.getHeader("WL-Proxy-Client-IP");
-        }
-        if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
-            ip = request.getHeader("HTTP_X_FORWARDED_FOR");
-        }
-        // 如果是多级代理，那么取第一个ip为客户ip
-        if (ip != null && ip.indexOf(",") != -1) {
-            ip = ip.substring(ip.lastIndexOf(",") + 1, ip.length()).trim();
-        }
-        return ip;
+//        String ip = request.getHeader("x-forwarded-for");
+//        if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
+//            ip = request.getHeader("http_client_ip");
+//        }
+//        if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
+//            ip = request.getRemoteAddr();
+//        }
+//        if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
+//            ip = request.getHeader("Proxy-Client-IP");
+//        }
+//        if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
+//            ip = request.getHeader("WL-Proxy-Client-IP");
+//        }
+//        if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
+//            ip = request.getHeader("HTTP_X_FORWARDED_FOR");
+//        }
+//        // 如果是多级代理，那么取第一个ip为客户ip
+//        if (ip != null && ip.indexOf(",") != -1) {
+//            ip = ip.substring(ip.lastIndexOf(",") + 1, ip.length()).trim();
+//        }
+//        return ip;
+        //由于同一个公司的外网ip是同一个，不利于计算访问量，所有用sessionid来代替
+        return request.getSession().getId();
     }
 }
